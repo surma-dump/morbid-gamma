4fbc7d1353e5f3b7b0a9be087fe22a63933c0ce9
Alexander Surma <surma@surmair.de>
2013-02-26 19:59:02 +0100

    Use english and more parameters

--- morbid_gamma.ino
+++ morbid_gamma.ino
@@ -1,10 +1,16 @@
-// Abstaende zwischen Ausloesern in Millisekunden
-// Maximum 4294967295 =~ 49 Tage
-#define PRESS_INTERVAL 8000
-#define PRESS_DURATION 500
+// Duration between trigger presses
+// Maximum: 4294967295 ms =~ 50 days
+#define TRIGGER_INTERVAL 8000
+#define TRIGGER_DURATION 500
+// Number of degrees to turn
+#define ANGULAR_DISTANCE 15
+#define INVERT 0
 
 #define SERVO_PIN 8
 #define LED_PIN 13
+
+#define UNTRIGGER_POS (INVERT?ANGULAR_DISTANCE:0)
+#define TRIGGER_POS (INVERT?0:ANGULAR_DISTANCE)
 
 #include <Servo.h>
 
@@ -12,18 +18,18 @@ Servo servo;
 
 void setup() {
   servo.attach(SERVO_PIN);
-  servo.write(0);
+  servo.write(UNTRIGGER_POS);
   digitalWrite(LED_PIN, LOW);
 }
 
 
 void loop() {
   while(true) {
-    servo.write(45);
+    servo.write(TRIGGER_POS);
     digitalWrite(LED_PIN, HIGH);
-    delay(PRESS_DURATION);
-    servo.write(0);
+    delay(TRIGGER_DURATION);
+    servo.write(UNTRIGGER_POS);
     digitalWrite(LED_PIN, LOW);
-    delay(PRESS_INTERVAL);
+    delay(TRIGGER_INTERVAL);
   }
 }
